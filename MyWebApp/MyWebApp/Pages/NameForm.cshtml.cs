69c2e3ca90a4e7618cbcdd999b5438f71f74794d
StewartBarker <stew.barker@outlook.com>
2021-11-02 11:17:20 +0000

    completed task

---
+++ MyWebApp/MyWebApp/Pages/NameForm.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class NameFormModel : PageModel
+    {
+        public string testField { get; set; }
+
+        public void OnGet()
+        {
+            testField = System.DateTime.Now.ToString();
+        }
+
+        public IActionResult OnPost(string getName)
+        {
+            return RedirectToPage("./Hello", new { name = getName });
+        }
+    }
+}
